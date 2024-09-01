62c4deebb0621374b2688f8070680fa228fdab99
Ahnaf Shikder Arnob <ahnafarnob31@gmail.com>
2024-09-02 03:21:39 +0600

    Final Submission

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
    #path("january", views.january),
    #path("february", views.feb),
    #path("march", views.march),
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenges_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ] 
